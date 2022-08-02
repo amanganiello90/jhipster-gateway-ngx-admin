ae6a5a9e5fdb697447d5024f665a3d283e6dd8ea
Angelo Manganiello <angelo.mang@libero.it>
2022-08-02 09:41:44 +0200

    Initial version of gatewayskipclient generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/gateway/skipclient/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.gateway.skipclient.repository;
+
+import com.gateway.skipclient.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
